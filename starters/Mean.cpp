f8e4af478df0302f3e87aa6830e5c885b9fd45d2
Snigdha Gupta <80680269+snigdha2808@users.noreply.github.com>
2022-01-27 23:35:06 +0530

    Add files via upload

---
+++ starters/Mean.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int mean;
+        cin>>mean;
+        cout<<mean-1<<" "<<mean<<" "<<mean+1<<endl;
+    }
+}
